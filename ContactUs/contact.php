cabaf32f02bcec02efccad4a25a90ef9f72e811f
anox <sasegoh591@naymeo.com>
2021-09-18 19:51:05 +0530

    first commit

---
+++ ContactUs/contact.php
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+        <title>Contact Us</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <link rel="stylesheet" href="css/creative.css" type="text/css">
+    </head>
+    
+    <body id="page-top">
+        
+        <nav id="mainNav" class="navbar navbar-default navbar-fixed-top" style="background:black">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="http://www.hgce.org">E-Placement</a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a class="page-scroll" href="../Profilers/Login/index.php">Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../../AboutUs/about.php">About Us</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../ContactUs/contact.php">Contact Us</a>
+                        </li>
+                    </ul>
+                
+				</div>
+            </div>
+        </nav>
+        <header>
+            <div class="header-content">
+                <div class="header-content-inner">
+                    <h1>Contact Us</h1>
+                    <hr>
+                    <p>HGCE, Vahelal</p>
+					<p>Ahmedabad, Gujarat</p>
+					<p>+91-7622008102</p>
+                    <a href="mailto:placement@rediffmail.com" class="btn btn-primary btn-xl page-scroll">MAIL US</a>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
